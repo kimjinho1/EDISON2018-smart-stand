030b89188021d8db986ce20543404e67a4683a93
kimjinho1 <rlawlsgh8113@naver.com>
2018-07-11 23:00:00 +0900

    Update wea.py

--- wea.py
+++ wea.py
@@ -1,3 +1,7 @@
-import time
+from pyowm import OWM
+API_key = 'b19cc0c64f569ba9eefd6b3e9ff1b2eb'
+owm = OWM(API_key)
+obs=owm.weather_at_id(1843564)
+w=obs.get_weather()
 
-print(time.ctime())
+print(w.get_status(),w.get_temperature(unit='celsius')['temp'])
